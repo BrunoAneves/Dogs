bb12c12c1bc823bc689817f9c258a768f691ae4d
Bruno-Araujo <101733555+Bruno-kitrim@users.noreply.github.com>
2023-05-11 10:15:10 -0300

    Correção Error.js

--- src/Components/Helper/Error.js
+++ src/Components/Helper/Error.js
@@ -3,7 +3,7 @@ import styles from "./Error.module.css";
 
 const Error = ({ error }) => {
   if (!error) return null;
-  return <p className={styles.error}></p>;
+  return <p className={styles.error}>{error}</p>;
 };
 
 export default Error;
