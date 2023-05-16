43aa81ba08b11a90cd65ccab994ec221f321ca25
Bruno-Araujo <101733555+Bruno-kitrim@users.noreply.github.com>
2023-05-16 10:22:50 -0300

    User Header

---
+++ src/Components/Feed/Feed.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserHeaderNav from "../User/UserHeaderNav";
+
+const Feed = () => {
+  return (
+    <header>
+      <h1>Titulo</h1>
+    </header>
+  );
+};
+
+export default Feed;
