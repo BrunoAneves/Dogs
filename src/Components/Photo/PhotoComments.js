e956f0fd0831f7b0a09f11eecc3153339d93330c
Bruno-Araujo <101733555+Bruno-kitrim@users.noreply.github.com>
2023-07-24 19:24:12 -0300

    Comentário Estilo

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -5,11 +5,15 @@ import styles from "./PhotoComments.module.css";
 
 const PhotoComments = (props) => {
   const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
   const { login } = useContext(UserContext);
 
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
   return (
     <>
-      <ul style={styles.comments}>
+      <ul ref={commentsSection} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>
